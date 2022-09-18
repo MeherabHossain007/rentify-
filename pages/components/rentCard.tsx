461c040b01bba26ae9641e26f8386350b8271f68
MeherabHossain007 <meherab788@gmail.com>
2022-09-18 22:58:08 +0600

    search screen added

--- pages/components/rentCard.tsx
+++ pages/components/rentCard.tsx
@@ -1,5 +1,6 @@
 import {
   Badge,
+  Box,
   Button,
   Center,
   Flex,
@@ -9,6 +10,9 @@ import {
   Image,
   Link,
   Stack,
+  Tag,
+  TagLabel,
+  TagRightIcon,
   Text,
   useBoolean,
   useColorModeValue,
@@ -18,6 +22,7 @@ import { FaBed, FaBath, FaBorderAll } from "react-icons/fa";
 import React, { Component, ReactDOM, useEffect, useState } from "react";
 import router from "next/router";
 import AlertMassage from "./alert";
+import { IoLocationSharp } from "react-icons/io5";
 export default function RentCard({
   amount,
   location,
@@ -27,8 +32,9 @@ export default function RentCard({
   bath,
   area,
   image,
-  post_id,
+  id,
   phone,
+  children
 }) {
   var isBooked: boolean;
   const [bookId, setId] = useState("");
@@ -42,108 +48,111 @@ export default function RentCard({
   };
   return (
     <div>
-      <Center
-        py={6}
-        as={"button"}
-        onClick={() => {
-          router.push({
-            pathname: "/components/rentDetails",
-            query: post_id,
-          });
-        }}
+    <Center>
+      <Stack
+        borderWidth="1px"
+        borderRadius="lg"
+        w={"70%"}
+        height={"100%"}
+        bg={useColorModeValue("white", "gray.900")}
+        boxShadow={"lg"}
+        padding={4}
       >
-        <Stack
-          borderWidth="1px"
-          borderRadius="lg"
-          w={"70%"}
-          height={"45vh"}
-          direction={{ base: "column", md: "row" }}
-          bg={useColorModeValue("white", "gray.900")}
-          boxShadow={"lg"}
-          padding={4}
-        >
-          <Flex flex={1} bg="blue.200" borderRadius={"lg"}>
-            <Image
-              objectFit="cover"
-              boxSize="100%"
-              borderRadius={"lg"}
-              src={image}
-            />
-          </Flex>
-          <Stack
-            flex={1}
-            flexDirection="column"
-            justifyContent="start"
-            alignItems="start"
-            p={1}
-            pl={10}
+        <Center py={6}>
+          <Box
+            borderWidth="1px"
+            borderRadius="lg"
+            w={"100%"}
+            height={"100%"}
+            bg={useColorModeValue("white", "gray.900")}
+            boxShadow={"lg"}
+            padding={4}
+            as={"button"}
+            onClick={() => {
+              router.push({
+                pathname: "/components/ProCardDetails",
+                query: id,
+              });
+            }}
           >
-            <Stack direction="row" alignItems="baseline">
-              <Text fontSize="18" fontWeight="bold" textAlign={"match-parent"}>
-                BDT
-              </Text>
-              <Text fontSize="40" fontWeight="bold">
-                {amount}
-              </Text>
-            </Stack>
-            <Text>{location}</Text>
-            <Badge>{type}</Badge>
-            <Heading
-              fontSize={20}
-              overflow={"hidden"}
-              orientation={"horizontal"}
-              noOfLines={1}
-            >
-              {title}
-            </Heading>
-            <HStack spacing={7} pt={3} alignItems={"baseline"}>
-              <HStack>
-                <FaBed size={20} />
-                <Text>{bed}</Text>
-              </HStack>
-              <HStack>
-                <FaBath size={20} />
-                <Text>{bath}</Text>
-              </HStack>
-              <HStack>
-                <FaBorderAll size={20} />
-                <Text>{area}</Text>
-              </HStack>
-            </HStack>
-            <Flex justifyContent={"flex-end"} flexDirection={"column"} flex={1}>
-              <HStack py={4} justifyContent={"start"}>
-                <AlertMassage
-                  bname={"Call"}
-                  text={phone}
-                  header={"Phone Number"}
-                />
-                <Button
-                  as={"a"}
-                  w={100}
-                  fontSize={"sm"}
-                  fontWeight={400}
-                  color="white"
-                  variant={"solid"}
-                  bg="green.400"
-                  _hover={{
-                    bg: "green.400",
-                  }}
-                  href={"/signUp"}
+            <Stack direction={{ base: "column", md: "row" }}>
+              <Image
+                objectFit="cover"
+                maxH={"30%"}
+                maxW={"50%"}
+                borderRadius={"lg"}
+                src={image}
+              />
+              <Stack
+                flex={1}
+                flexDirection="column"
+                justifyContent="start"
+                alignItems="start"
+                p={1}
+                pl={10}
+              >
+                <Stack direction="row" alignItems="baseline">
+                  <Text
+                    fontSize="md"
+                    fontWeight="bold"
+                    textAlign={"match-parent"}
+                  >
+                    BDT
+                  </Text>
+                  <Text fontSize="4xl" fontWeight="bold">
+                    {amount}
+                  </Text>
+                </Stack>
+                <Tag size={"lg"} variant="outline" colorScheme="blue">
+                  <TagLabel>{location}</TagLabel>
+                  <TagRightIcon boxSize="12px" as={IoLocationSharp} />
+                </Tag>
+                <Badge fontSize="sm" colorScheme="blue" variant={"solid"}>
+                  {type}
+                </Badge>
+                <Heading
+                  fontSize={20}
+                  overflow={"hidden"}
+                  orientation={"horizontal"}
+                  noOfLines={1}
+                  textAlign={"start"}
                 >
-                  Email
-                </Button>
-              </HStack>
-            </Flex>
-          </Stack>
-          {bookId === post_id ? (
-            <Badge colorScheme="green" h={5}>
-              Booked
-            </Badge>
-          ) : (
-            ""
-          )}
-        </Stack>
-      </Center>
+                  {title}
+                </Heading>
+                <HStack spacing={7} pt={2} alignItems={"baseline"}>
+                  <HStack>
+                    <FaBed size={20} />
+                    <Text>{bed}</Text>
+                  </HStack>
+                  <HStack>
+                    <FaBath size={20} />
+                    <Text>{bath}</Text>
+                  </HStack>
+                  <HStack>
+                    <FaBorderAll size={20} />
+                    <Text>{area}</Text>
+                  </HStack>
+                </HStack>
+                <Flex
+                  justifyContent={"flex-end"}
+                  flexDirection={"column"}
+                  flex={1}
+                >
+                  <HStack justifyContent={"start"}>{children}</HStack>
+                </Flex>
+              </Stack>
+              {bookId === id ? (
+                <Badge colorScheme="green" h={5}>
+                  Booked
+                </Badge>
+              ) : (
+                ""
+              )}
+            </Stack>
+          </Box>
+        </Center>
+      </Stack>
+    </Center>
     </div>
   );
 }
---
+++ pages/screens/searchScreen.tsx
@@ -0,0 +1,304 @@
+import { SearchIcon } from "@chakra-ui/icons";
+import {
+  Stack,
+  Flex,
+  Button,
+  Text,
+  VStack,
+  useBreakpointValue,
+  Box,
+  HStack,
+  FormControl,
+  FormLabel,
+  Input,
+  IconButton,
+  Center,
+  Wrap,
+  WrapItem,
+  Heading,
+  useColorModeValue,
+  useRadio,
+  useRadioGroup,
+  Badge,
+  Select,
+  Container,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { FC, SetStateAction, useEffect, useState } from "react";
+import HomeCard from "../components/catagoryCard";
+import Navbar from "../components/navBar";
+import RentCard from "../components/rentCard";
+
+const Card = ({ children }: any) => {
+  return (
+    <Box
+      bgColor={"white"}
+      borderRadius={10}
+      boxShadow={"lg"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      h={100}
+      w={"100%"}
+      p={4}
+    >
+      {children}
+    </Box>
+  );
+};
+
+const SearchCard = ({ children }: any) => {
+  return (
+    <Box
+      bgColor={"white"}
+      borderRadius={10}
+      boxShadow={"lg"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      h={100}
+      w={"md"}
+      p={4}
+      my={4}
+    >
+      {children}
+    </Box>
+  );
+};
+
+const api = axios.create({
+  baseURL: `http://localhost:5001/api/post`,
+});
+
+export default function SearchScreen() {
+  const [location, setLocation] = useState("");
+  const [price, setPrice] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [type, setType] = useState("");
+
+  const handleSearch = async () => {
+    let data = await api.get("/").then(({ data }) => data);
+    setPosts(data);
+  };
+
+  const RadioCard = (props) => {
+    const { getInputProps, getCheckboxProps } = useRadio(props);
+    const input = getInputProps();
+    const checkbox = getCheckboxProps();
+
+    return (
+      <Box as="label">
+        <input {...input} />
+        <Box
+          {...checkbox}
+          cursor="pointer"
+          borderRadius="md"
+          _checked={{
+            bg: "blue.600",
+            color: "white",
+            borderColor: "blue.600",
+          }}
+          _focus={{
+            boxShadow: "outline",
+          }}
+          px={5}
+          py={3}
+          w={"263px"}
+          bg={useColorModeValue("white", "gray.800")}
+          boxShadow={"lg"}
+          _hover={{
+            boxShadow: "2xl",
+          }}
+          rounded={"lg"}
+          pos={"relative"}
+          display={"flex"}
+          flexDirection="column"
+          alignItems="center"
+        >
+          {props.children}
+        </Box>
+      </Box>
+    );
+  };
+
+  const options = ["Hostel", "Apartment", "Roommate", "Sublet"];
+
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: "framework",
+    defaultValue: "Apartment",
+    onChange: setType,
+  });
+
+  const group = getRootProps();
+
+  const Post = ({ posts }) => {
+    console.log(posts);
+    return (
+      <Flex flex={1} flexDirection={"row"}>
+        <Wrap>
+          {posts.map((posts) =>
+            posts.approval_status === "True" ? (
+              posts.location === location ? (
+                posts.type === type ? (
+                  Number(posts.price) <= Number(price) ? (
+                    <WrapItem>
+                      <RentCard
+                        amount={posts.price}
+                        location={posts.location}
+                        type={posts.type}
+                        title={posts.name}
+                        bed={posts.beds}
+                        bath={posts.baths}
+                        area={posts.area}
+                        phone={posts.number}
+                        image={
+                          "https://firebasestorage.googleapis.com/v0/b/rentify-4f59b.appspot.com/o/colonial-style-house-night-scene.jpg?alt=media&token=6bfee092-54bc-4c68-904b-5d7af87a78c0"
+                        }
+                        id={posts.id}
+                        children={undefined}
+                      />
+                    </WrapItem>
+                  ) : (
+                    ""
+                  )
+                ) : (
+                  ""
+                )
+              ) : (
+                " "
+              )
+            ) : (
+              " "
+            )
+          )}
+        </Wrap>
+      </Flex>
+    );
+  };
+  return (
+    <>
+      <Navbar />
+      <Container
+        bgImage="url('https://firebasestorage.googleapis.com/v0/b/rentify-4f59b.appspot.com/o/rentify%20reloaded%2F2201_w039_n003_98b_p1_98.jpg?alt=media&token=f3176961-d18f-4c97-ad45-2e48db68645d')"
+        bgSize={"867px"}
+        maxW={"full"}
+        h={"45vh"}
+        justifyContent={"center"}
+        display={"flex"}
+        flexDirection="row"
+        alignItems="center"
+        flexGrow={1}
+      >
+        <Center>
+          <Flex
+            w={"100%"}
+            borderRadius={30}
+            backgroundSize={"cover"}
+            backgroundPosition={"center center"}
+          >
+            <VStack
+              w={"full"}
+              justify={"center"}
+              px={useBreakpointValue({ base: 4, md: 8 })}
+              borderRadius={30}
+              justifyContent={"center"}
+            >
+              <Box
+                bgColor={"white"}
+                boxSize={"400px"}
+                boxShadow={"2xl"}
+                w={"100%"}
+                h={"100%"}
+                borderRadius={10}
+                display={"flex"}
+                flexDirection="column"
+                justifyContent={"flex-start"}
+                p={5}
+              >
+                <Heading fontSize={"2xl"}>Search Rent in Dhaka</Heading>
+                <Text>
+                  Enter your location to see the latest prices and deals for
+                  Dhaka hotels
+                </Text>
+                <HStack
+                  display={"flex"}
+                  flexDirection="row"
+                  justifyContent={"center"}
+                >
+                  <SearchCard>
+                    <Stack
+                      direction={"column"}
+                      spacing={2}
+                      alignSelf={"stretch"}
+                    >
+                      <Flex
+                        alignContent={"center"}
+                        direction={"row"}
+                        flex={1}
+                        gap={2}
+                      >
+                        <FormControl>
+                          <FormLabel htmlFor="location">Location</FormLabel>
+                          <Select
+                            id="location"
+                            placeholder="Select location"
+                            onChange={(e) => setLocation(e.target.value)}
+                          >
+                            <option>Badda</option>
+                            <option>Gulshan</option>
+                            <option>Banani</option>
+                            <option>Rampura</option>
+                            <option>Baridhara</option>
+                          </Select>
+                        </FormControl>
+                      </Flex>
+                    </Stack>
+                  </SearchCard>
+                  <Card>
+                    <FormControl>
+                      <HStack justifyContent={"space-between"}>
+                        <FormLabel htmlFor="amount">Price</FormLabel>
+                        <Badge colorScheme="blue" variant={"solid"}>
+                          MAX
+                        </Badge>
+                      </HStack>
+                      <Input
+                        id="price"
+                        onChange={(e) => setPrice(parseInt(e.target.value))}
+                      />
+                    </FormControl>
+                  </Card>
+                </HStack>
+                <VStack>
+                  <HStack {...group}>
+                    {options.map((value) => {
+                      const radio = getRadioProps({ value });
+                      return (
+                        <RadioCard key={value} {...radio}>
+                          {value}
+                        </RadioCard>
+                      );
+                    })}
+                  </HStack>
+                  <IconButton
+                    w={"full"}
+                    m={"3"}
+                    colorScheme="blue"
+                    aria-label="Search database"
+                    icon={<SearchIcon />}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleSearch();
+                    }}
+                  />
+                </VStack>
+              </Box>
+            </VStack>
+          </Flex>
+        </Center>
+      </Container>
+
+      <VStack justify="center" pl={170} pr={170}>
+        <Post posts={posts} />
+      </VStack>
+    </>
+  );
+}
